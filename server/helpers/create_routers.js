a3f13303711f346cdf36685525b053e3b6140fa7
MackieJG <joshuamackie10@googlemail.com>
2023-01-20 19:03:09 +0000

    updating routes

---
+++ server/helpers/create_routers.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const ObjectID = require('mongodb').ObjectID
+
+const createRouter = function (collection) {
+
+
+    const router = express.Router()
+  // handle index route 
+
+    router.get('/' , (req, res) => {
+        collection
+        .find()
+        .toArray()
+        .then((docs) => res.json(docs))
+        .catch((err) => {
+            console.log(err);
+            res.status(500)
+            res.json({status: 500, error: err})
+        })
+    })
+  // handle post request
+
+    router.post('/', (req, res) => {
+        const newStatsData = req.body
+        collection
+        .insertOne(newStatsData)
+        .then((result) => {
+            res.json(result.ops[0])
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500)
+            res.json({status:500, error:err})
+        })
+    })
+// delete
+
+    router.delete('/:id', (req, res) => {
+        const id = req.params.id
+        collection
+        .deleteOne({_id: ObjectID(id) })
+        .then(result => {
+            res.json(result)
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500)
+            res.json({status:500, error:err})
+        })
+    })
+
+    router.put('/:id', (req, res) => {
+        const id = req.params.id;
+        const updatedData = req.body;
+  
+        collection
+        .updateOne({ _id: ObjectID(id) }, { $set: updatedData })
+        .then((result) => {
+            res.json(result)
+          })
+        .catch((err) => {
+            console.log(err);
+            res.status(500)
+            res.json({status:500, error:err})
+        })
+      });
+
+    return router
+}
+
+module.exports = createRouter
