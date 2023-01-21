c6c6b71b90ce8cd1af86d3342ce0eb8ddc963c38
Brian Dickson <brianlawrencedickson@gmail.com>
2023-01-21 18:39:42 +0000

    added NavBar, logo, installed Routes and Inline Styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import JournalContainer from './Containers/JournalContainer';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from './Components/NavBar';
 import './App.css';
+import Home from "./Components/Home";
+import JournalForm from './Components/JournalForm';
+import JournalList from './Components/JournalList';
+
 
 function App() {
   return (
-    <JournalContainter />
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/create' element={<JournalForm />} />
+        <Route path='/journal' element={<JournalList />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/Components/NavBar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+
+const NavBarContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    gap: 2rem;
+    align-items: center;
+    padding: 1rem;
+    justify-content: center;
+
+    > div {
+    min-width: 280px;
+    display: flex;
+    gap: 2rem;
+    align-items: center;
+    }
+`
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #4a4a4a;
+  font-size: 20px;
+  font-weight: 600;
+  align-items: center;
+`
+
+
+const NavBar = () => {
+
+  return (
+    <NavBarContainer>
+        <div>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/journal">Make an Entry</StyledLink>
+        <StyledLink to="/stats">How am I doing?</StyledLink>
+        </div>
+    </NavBarContainer>
+  );
+}
+
+export default NavBar;
