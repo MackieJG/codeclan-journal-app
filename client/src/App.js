00a51345534d3fbacfdae4ea1da40d4cd47997c3
kylefenlon <kyle@kfenlon.com>
2023-01-22 14:29:31 +0000

    merged the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from './Components/NavBar';
+
 import './App.css';
-import Home from "./Components/Home";
-import JournalForm from './Components/JournalForm';
-import JournalList from './Components/JournalList';
+import JournalContainer from './Containers/JournalContainer'
 
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/create' element={<JournalForm />} />
-        <Route path='/journal' element={<JournalList />} />
-      </Routes>
-    </Router>
+    <JournalContainer />
   );
 }
 
