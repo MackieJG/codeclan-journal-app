3aa9205a5eb88431151f599046575a220fef4bbf
kylefenlon <kyle@kfenlon.com>
2023-01-22 15:24:24 +0000

    finished the list

---
+++ client/src/Components/JournalDetail.js
@@ -0,0 +1,31 @@
+import { deleteEntry, putEntry } from './JournalService';
+
+const JournalDetail = ({ entry, removeEntry, updateEntry }) => {
+
+    const handleUpdate = () => {
+        const updatedEntry = { ...entry };
+        putEntry(updatedEntry)
+            .then((data) => {
+                updateEntry(data);
+            })
+    }
+
+    const handleDelete = () => {
+        deleteEntry(entry._id)
+            .then(() => {
+                removeEntry(entry._id)
+            })
+    };
+
+    return (
+        <>
+            <p>Journal Entry Date: {entry.date}</p>
+            {/* <p>Checked in? {entry.checked_in ? 'true' : 'false'}</p> */}
+            {/* <button onClick={() => handleUpdate()}>Check {entry.checked_in ? 'Out' : 'In'}</button> */}
+            <button onClick={() => handleDelete()}> 🗑 </button>
+            <hr />
+        </>
+    )
+};
+
+export default JournalDetail;
--- client/src/Components/JournalList.js
+++ client/src/Components/JournalList.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import JournalDetail from "./JournalDetail";
 
-const JournalList = () => {
+const JournalList = ({ journalEntries, removeEntry, updateEntry }) => {
+    if (!journalEntries) return <h2>Loading...</h2>
+    const journalList = journalEntries.map((entry) => {
+        return (
+            <JournalDetail entry={entry} removeEntry={removeEntry} key={entry._id} updateEntry={updateEntry} />
+        )
+    });
     return (
-        <div>JournalList</div>
+        <>
+            {journalList}
+        </>
     )
 }
 
