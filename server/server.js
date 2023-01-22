e6fb168ef4ec1a3dc13ab66952ac0bccfe553575
kylefenlon <kyle@kfenlon.com>
2023-01-22 14:06:05 +0000

    finished first form

--- server/server.js
+++ server/server.js
@@ -11,12 +11,12 @@ const createRouter = require('./helpers/create_routers.js');
 MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
     .then((client) => {
         const db = client.db('codeclan_journal');
-        const bookingsCollection = db.collection('entries');
-        const bookingsRouter = createRouter(bookingsCollection);
-        app.use('/api/entries', bookingsRouter);
+        const entriesCollection = db.collection('entries');
+        const entriesRouter = createRouter(entriesCollection);
+        app.use('/api/entries', entriesRouter);
     })
     .catch(console.error);
 
-    app.listen(9000, function () {
-        console.log(`Listening on Port ${ this.address().port}`);
-    });
+app.listen(9000, function () {
+    console.log(`Listening on Port ${this.address().port}`);
+});
