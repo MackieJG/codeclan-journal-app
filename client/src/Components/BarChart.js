08af0cbf1121aee432697e596f7f8f7e74ae55a8
kylefenlon <kyle@kfenlon.com>
2023-01-24 13:50:35 +0000

    finished

--- client/src/Components/BarChart.js
+++ client/src/Components/BarChart.js
@@ -1,79 +1,79 @@
-// import {
-//     Chart as ChartJS,
-//     CategoryScale,
-//     LinearScale,
-//     BarElement,
-//     Title,
-//     Tooltip,
-//     Legend,
-// } from 'chart.js';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
 
-// import { Bar } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 
-// ChartJS.register(
-//     CategoryScale,
-//     LinearScale,
-//     BarElement,
-//     Title,
-//     Tooltip,
-//     Legend
-// );
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
-// const BarChart = () => {
-//     const labels = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-//     const data = {
-//         labels,
-//         datasets: [
-//             {
-//                 label: 'sleep-stats',
-//                 data: [2, 3, 5, 4, , 2, 3],
-//                 backgroundColor: 'rgba(187, 9, 132, 0.5)',
-//             },
-//             {
-//                 label: 'meal-stats',
-//                 data: [1, 3, 5, 4, 5, 4, 5],
-//                 backgroundColor: 'rgba(81, 89, 132, 0.5)',
-//             },
-//             {
-//                 label: 'screen-stats',
-//                 data: [1, 5, 1, 3, 1, 4, 2],
-//                 backgroundColor: 'rgba(25, 88, 102, 0.5)',
-//             },
-//             {
-//                 label: 'downtime-stats',
-//                 data: [2, 2, 1, 2, 5, 2, 4],
-//                 backgroundColor: 'rgba(125, 99, 132, 0.5)',
-//             },
-//             {
-//                 label: 'mood-stats',
-//                 data: [3, 5, 3, 4, 5, 3, 4],
-//                 backgroundColor: 'rgba(180, 79, 112, 0.5)',
-//             },
-//             {
-//                 label: 'hydration-stats',
-//                 data: [2, 2, 2, 1, 2, 4, 5],
-//                 backgroundColor: 'rgba(255, 99, 132, 0.5)',
-//             }
-//         ]
-//     }
+const BarChart = () => {
+    const labels = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'sleep-stats',
+                data: [2, 3, 5, 4, , 2, 3],
+                backgroundColor: 'rgba(187, 9, 132, 0.5)',
+            },
+            {
+                label: 'meal-stats',
+                data: [1, 3, 5, 4, 5, 4, 5],
+                backgroundColor: 'rgba(81, 89, 132, 0.5)',
+            },
+            {
+                label: 'screen-stats',
+                data: [1, 5, 1, 3, 1, 4, 2],
+                backgroundColor: 'rgba(25, 88, 102, 0.5)',
+            },
+            {
+                label: 'downtime-stats',
+                data: [2, 2, 1, 2, 5, 2, 4],
+                backgroundColor: 'rgba(125, 99, 132, 0.5)',
+            },
+            {
+                label: 'mood-stats',
+                data: [3, 5, 3, 4, 5, 3, 4],
+                backgroundColor: 'rgba(180, 79, 112, 0.5)',
+            },
+            {
+                label: 'hydration-stats',
+                data: [2, 2, 2, 1, 2, 4, 5],
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            }
+        ]
+    }
 
-//     const options = {
-//         responsive: true,
-//         plugins: {
-//             legend: { position: 'top' },
-//             title: { display: true, text: '.self-summary' }
-//         }
-//     }
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: { position: 'top' },
+            title: { display: true, text: '.self-summary' }
+        }
+    }
 
-//     return (
-//         <>
+    return (
+        <>
 
-//             <Bar options={options} data={data} />
+            <Bar options={options} data={data} />
 
-//         </>
-//     )
+        </>
+    )
 
-// };
+};
 
-// export default BarChart;
+export default BarChart;
 
--- client/src/Components/LineChart.js
+++ client/src/Components/LineChart.js
@@ -1,55 +1,55 @@
-// import {
-//     Chart as ChartJS,
-//     CategoryScale,
-//     LinearScale,
-//     PointElement,
-//     LineElement,
-//     Title,
-//     Tooltip,
-//     Legend,
-// } from 'chart.js';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
 
-// import { Line } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 
-// ChartJS.register(
-//     CategoryScale,
-//     LinearScale,
-//     PointElement,
-//     LineElement,
-//     Title,
-//     Tooltip,
-//     Legend
-// );
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
-// const LineChart = () => {
-//     const labels = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-//     const data = {
-//         labels,
-//         datasets: [
-//             {
-//                 label: 'daily-total',
-//                 borderColour: 'rgba(187, 30, 132)',
-//                 backgroundColor: 'rgba(187, 9, 132, 0.5)',
-//                 data: [20, 23, 25, 9, 18, 17, 22, 20, 23, 25, 9, 18, 17, 22],
-//             }]
-//     };
+const LineChart = () => {
+    const labels = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'daily-total',
+                borderColour: 'rgba(187, 30, 132)',
+                backgroundColor: 'rgba(187, 9, 132, 0.5)',
+                data: [20, 23, 25, 9, 18, 17, 22, 20, 23, 25, 9, 18, 17, 22],
+            }]
+    };
 
-//     const options = {
-//         responsive: true,
-//         plugins: {
-//             legend: { position: 'top' },
-//             title: { display: true, text: '.self-summary' }
-//         }
-//     }
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: { position: 'top' },
+            title: { display: true, text: '.self-summary' }
+        }
+    }
 
-//     return (
-//         <>
-//             <Line options={options} data={data} />
-//         </>
-//     )
-// };
+    return (
+        <>
+            <Line options={options} data={data} />
+        </>
+    )
+};
 
-// export default LineChart;
+export default LineChart;
 
 
 
